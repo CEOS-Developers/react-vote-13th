1071500a1a9c8bd59224b5a68d2bc90ee77ab83a
park ye jin <dyddyd0514@naver.com>
2021-05-13 00:22:12 +0900

    Feat: prettier & axios setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div > 12기 프론트엔드 개발팀장 투표 ^.^
-    </div>
-  );
+  return <div> 12기 프론트엔드 개발팀장 투표 ^.^</div>;
 }
 
 export default App;
