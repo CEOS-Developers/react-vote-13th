9781bacdf8436d6676d3308b6d5316f11bf5ee28
SojeongLee <sojunglsj@naver.com>
2021-05-13 16:26:29 +0900

    style : overall display and decoration

--- src/VoteButton.js
+++ src/VoteButton.js
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
+const StyledButton = styled.button`
+  display: inline-flex;
+  border: none;
+  border-radius: 4px;
+  background-color: #bf80ff;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  margin-left: 2rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+`;
 
-function VoteButton(props){
-    const [count, setCount] = useState(null);
-    const CastVote = async () => {
-        const id=props.vote_id;
-        try {
-          const data = await axios.get(
-            `http://ec2-13-209-5-166.ap-northeast-2.compute.amazonaws.com:8000/api/vote?id=${id}`
-          );
-          setCount(data);
-        } catch (e) {
-          alert("투표 에러가 발생했습니다.");
-        }
-        props.fetch();
-        alert('투표가 반영되었습니다.');
-      };
-  return(
-      <button onClick={CastVote}>투표</button>
-  );
+function VoteButton(props) {
+  const [count, setCount] = useState(null);
+  const CastVote = async () => {
+    const id = props.vote_id;
+    try {
+      const data = await axios.get(
+        `http://ec2-13-209-5-166.ap-northeast-2.compute.amazonaws.com:8000/api/vote?id=${id}`
+      );
+      setCount(data);
+    } catch (e) {
+      alert('투표 에러가 발생했습니다.');
+    }
+    props.fetch();
+    alert('투표가 반영되었습니다.');
+  };
+  return <StyledButton onClick={CastVote}>투표</StyledButton>;
 }
 
 export default VoteButton;
--- src/VoteScreen.js
+++ src/VoteScreen.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import VoteButton from './VoteButton';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  font-size: 23px;
+  & li {
+    margin-top: 12px;
+    margin-bottom: 12px;
+  }
 
+  & rank {
+    margin-right: 1rem;
+  }
+`;
+
+const Header = styled.div`
+  font-size: 32px;
+  color: #b3b3ff;
+  padding: 2rem;
+`;
 
 function VoteScreen() {
   const [candidates, setCandidates] = useState(null);
@@ -17,7 +39,7 @@ function VoteScreen() {
       const response = await axios.get(
         'http://ec2-13-209-5-166.ap-northeast-2.compute.amazonaws.com:8000/api/candidates'
       );
-      response.data.sort((a, b) => ( b.voteCount - a.voteCount ));
+      response.data.sort((a, b) => b.voteCount - a.voteCount);
       setCandidates(response.data);
     } catch (e) {
       setError(e);
@@ -32,18 +54,22 @@ function VoteScreen() {
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
   if (!candidates) return null;
+
   return (
-    <>
-      <ul>
-        {candidates.map((person, index) => (
-          <li key={person.id}>
-            <rank> {index+1}위</rank>
-            {person.name}[{person.voteCount}표]         
-            <VoteButton vote_id={person.id} fetch={fetchCandidates}>투표</VoteButton>
-          </li>
-        ))}
-      </ul>
-      </>
+    <Wrapper>
+      <Header>대망의 CEOS 14기 프론트 팀장 투표</Header>
+      {candidates.map((person, index) => (
+        <li key={person.id}>
+          <rank>
+            {index + 1}위 {person.name}
+          </rank>
+          [{person.voteCount}표]
+          <VoteButton vote_id={person.id} fetch={fetchCandidates}>
+            투표
+          </VoteButton>
+        </li>
+      ))}
+    </Wrapper>
   );
 }
 
