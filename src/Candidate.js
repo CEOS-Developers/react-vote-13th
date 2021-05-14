34e0b20ef04e3873970d26293a42a8b5b97c0b5d
Bum <sbl133@naver.com>
2021-05-14 21:42:24 +0900

    feat: candidate, vote

---
+++ src/Candidate.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { useContext, useEffect } from 'react';
+import {
+  GET_VOTE_FAILURE,
+  GET_VOTE_REQUEST,
+  GET_VOTE_SUCCESS,
+  VoteContext,
+} from './Vote';
+
+const Candidate = ({ rank }) => {
+  const { data, dispatch } = useContext(VoteContext);
+
+  useEffect(() => {
+    // console.log(rank);
+    // console.log(data);
+  }, []);
+
+  const handleOnClick = async () => {
+    dispatch({ type: GET_VOTE_REQUEST });
+    try {
+      const response = await axios.get(
+        `http://ec2-13-209-5-166.ap-northeast-2.compute.amazonaws.com:8000/api/vote?id=${data[rank].id}
+            `
+      );
+      console.log(response.data);
+      dispatch({ type: GET_VOTE_SUCCESS, data: response.data });
+    } catch (err) {
+      dispatch({ type: GET_VOTE_FAILURE, error: err.response });
+    }
+  };
+
+  return (
+    <li>
+      <span>{rank + 1}위 </span>
+      <span>{data[rank].name}</span>
+      <span>{data[rank].voteCount}</span>
+      <button onClick={handleOnClick}>투표</button>
+    </li>
+  );
+};
+
+export default Candidate;
--- src/Vote.js
+++ src/Vote.js
@@ -1,105 +1,111 @@
-import React, {useReducer, createContext, useEffect} from 'react';
+import React, { useReducer, createContext, useEffect, useMemo } from 'react';
+import Candidate from './Candidate.js';
 import axios from 'axios';
 
 export const VoteContext = createContext({
-    data: [],
-    dispatch: () =>{},
-})
+  data: [],
+  flag: false,
+  dispatch: () => {},
+});
 
 const initialState = {
-    data: [],
-    getCandidatesLoading: false,
-    getCandidatesDone: false,
-    getCandidatesError: null,
-    getVoteLoading: false,
-    getVoteDone: false,
-    getVoteError: null,
-}
+  data: [],
+  getCandidatesLoading: false,
+  getCandidatesDone: false,
+  getCandidatesError: null,
+  getVoteLoading: false,
+  getVoteDone: false,
+  getVoteError: null,
+};
 
-export const GET_CANDIDATES_REQUEST = "GET_CANDIDATES_REQUEST";
-export const GET_CANDIDATES_SUCCESS = "GET_CANDIDATES_SUCCESS";
-export const GET_CANDIDATES_FAILURE = "GET_CANDIDATES_FAILURE";
+export const GET_CANDIDATES_REQUEST = 'GET_CANDIDATES_REQUEST';
+export const GET_CANDIDATES_SUCCESS = 'GET_CANDIDATES_SUCCESS';
+export const GET_CANDIDATES_FAILURE = 'GET_CANDIDATES_FAILURE';
 
-export const GET_VOTE_REQUEST = "GET_VOTE_REQUEST";
-export const GET_VOTE_SUCCESS = "GET_VOTE_SUCCESS";
-export const GET_VOTE_FAILURE = "GET_VOTE_FAILURE";
+export const GET_VOTE_REQUEST = 'GET_VOTE_REQUEST';
+export const GET_VOTE_SUCCESS = 'GET_VOTE_SUCCESS';
+export const GET_VOTE_FAILURE = 'GET_VOTE_FAILURE';
 
 const reducer = (state, action) => {
-    switch(action.type){
-        case GET_CANDIDATES_REQUEST:
-            return{
-                ...state,
-                getCandidatesLoading: true,
-                getCandidatesDone: false,
-                getCandidatesError: null,
-            }
-        case GET_CANDIDATES_SUCCESS:
-            return{
-                ...state,
-                getCandidatesLoading: false,
-                getCandidatesDone: true,
-                data: action.data,
-            }
-        case GET_CANDIDATES_FAILURE:
-            return{
-                ...state,
-                getCandidatesLoading: false,
-                getCandidatesError: action.error,
-            }
-        case GET_VOTE_REQUEST:
-            return{
-                ...state,
-                getVoteLoading: true,
-                getVoteDone: false,
-                getVoteError: null,
-            }
-        case GET_VOTE_SUCCESS:
-            return{
-                ...state,
-                getVoteLoading: false,
-                getVoteDone: true,
-            }
-        case GET_VOTE_FAILURE:
-            return{
-                ...state,
-                getVoteLoading: false,
-                getVoteError: action.error,
-            }
-        default:
-            return state;
-    }
-}
-
-
+  switch (action.type) {
+    case GET_CANDIDATES_REQUEST:
+      return {
+        ...state,
+        getCandidatesLoading: true,
+        getCandidatesDone: false,
+        getCandidatesError: null,
+      };
+    case GET_CANDIDATES_SUCCESS:
+      action.data.sort((a, b) => {
+        return b.voteCount - a.voteCount;
+      });
+      return {
+        ...state,
+        getCandidatesLoading: false,
+        getCandidatesDone: true,
+        data: [...action.data],
+      };
+    case GET_CANDIDATES_FAILURE:
+      return {
+        ...state,
+        getCandidatesLoading: false,
+        getCandidatesError: action.error,
+      };
+    case GET_VOTE_REQUEST:
+      return {
+        ...state,
+        getVoteLoading: true,
+        getVoteDone: false,
+        getVoteError: null,
+      };
+    case GET_VOTE_SUCCESS:
+      return {
+        ...state,
+        getVoteLoading: false,
+        getVoteDone: true,
+        flag: !state.flag,
+      };
+    case GET_VOTE_FAILURE:
+      return {
+        ...state,
+        getVoteLoading: false,
+        getVoteError: action.error,
+      };
+    default:
+      return state;
+  }
+};
 
 const Vote = () => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, flag } = state;
+  const value = useMemo(() => ({ data, dispatch }), [data]);
 
-    const fetchCandidates = async () => {
-        dispatch({type: GET_CANDIDATES_REQUEST});
-        try{
-            const response = await axios.get(
-                'http://ec2-13-209-5-166.ap-northeast-2.compute.amazonaws.com:8000/api/candidates'
-            );
-            dispatch({type: GET_CANDIDATES_SUCCESS, data: response.data})
-        } catch (err) {
-            dispatch({type: GET_CANDIDATES_FAILURE, error: err.response.data})
-        }
+  const fetchCandidates = async () => {
+    dispatch({ type: GET_CANDIDATES_REQUEST });
+    try {
+      const response = await axios.get(
+        'http://ec2-13-209-5-166.ap-northeast-2.compute.amazonaws.com:8000/api/candidates'
+      );
+      dispatch({ type: GET_CANDIDATES_SUCCESS, data: response.data });
+    } catch (err) {
+      dispatch({ type: GET_CANDIDATES_FAILURE, error: err.response });
     }
-    useEffect(()=>{
-        fetchCandidates();
-    }, []);
+  };
 
-    return(
-        <ul>
-            {state.data.map(candidate=>(
-                <li key={candidate.id}>
-                    {candidate.name}
-                </li>
-            ))}
-        </ul>
-    );
+  useEffect(() => {
+    fetchCandidates();
+  }, [flag]);
 
-}
+  return (
+    <VoteContext.Provider value={value}>
+      <ul>
+        {state.data.map((v, i) => (
+          <Candidate rank={i} />
+        ))}
+      </ul>
+    </VoteContext.Provider>
+  );
+};
 
 export default Vote;
