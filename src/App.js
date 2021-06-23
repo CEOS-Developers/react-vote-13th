3949d3b32378273ddeeffa9aadc3a0772293e273
park ye jin <dyddyd0514@naver.com>
2021-06-24 01:36:49 +0900

    Feat react-router-dom & route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import VoteView from './VoteView';
+import { Route } from 'react-router-dom';
+import SignUp from './Signup';
+import Signin from './Signin';
 
 function App() {
-  return <VoteView />;
+  return (
+    <div>
+      <Route exact path="/" component={VoteView} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={Signin} />
+    </div>
+  );
 }
 
 export default App;
